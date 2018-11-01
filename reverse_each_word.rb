58c9043b4ea7a82b76288d4da77b4adbb6779345
michaelstsnow <michael.st.snow@gmail.com>
2018-11-01 16:22:21 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(phase)
   broken_phrase=phrase.split(" ")
   final_phrase=""
-  broken_phrase.each do { |word|
+  broken_phrase.each { |word|
     final_phrase = "#{final_phrase} #{word}"
   }
   end
